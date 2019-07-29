54b7e748ea13eb96b27ac0fc302b1feaf038dc9b
Ilya Makhon <IlyaMakhon@coherentsolutions.com>
2019-07-29 17:56:54 +0300

    models ref with ESLint

--- app/model/building.js
+++ app/model/building.js
@@ -1,23 +1,23 @@
 export default class Building {
-    constructor(numberOfFloors, numberOfEntrances, numberOfApartmentsOnFloor) {
-        this.numberOfFloors = numberOfFloors;
-        this.numberOfEntrances = numberOfEntrances;
-        this.numberOfApartmentsOnFloor = numberOfApartmentsOnFloor;
-    }
+	constructor(numberOfFloors, numberOfEntrances, numberOfApartmentsOnFloor) {
+		this.numberOfFloors = numberOfFloors;
+		this.numberOfEntrances = numberOfEntrances;
+		this.numberOfApartmentsOnFloor = numberOfApartmentsOnFloor;
+	}
 
-    set setNumberOfFloors(numberOfFloors) {
-        this.numberOfFloors = numberOfFloors;
-    }
+	set setNumberOfFloors(numberOfFloors) {
+		this.numberOfFloors = numberOfFloors;
+	}
 
-    set setNumberOfEntrances(numberOfEntrances) {
-        this.numberOfEntrances = numberOfEntrances;
-    }
+	set setNumberOfEntrances(numberOfEntrances) {
+		this.numberOfEntrances = numberOfEntrances;
+	}
 
-    set setNumberOfApartmentsOnFloor(numberOfApartmentsOnFloor) {
-        this.numberOfApartmentsOnFloor = numberOfApartmentsOnFloor;
-    }
+	set setNumberOfApartmentsOnFloor(numberOfApartmentsOnFloor) {
+		this.numberOfApartmentsOnFloor = numberOfApartmentsOnFloor;
+	}
 
-    set setWholeAmountOfApartments(wholeAmountOfApartments) {
-        this.wholeAmountOfApartments = wholeAmountOfApartments;
-    }
+	set setWholeAmountOfApartments(wholeAmountOfApartments) {
+		this.wholeAmountOfApartments = wholeAmountOfApartments;
+	}
 }
--- app/taskgroup3/task3_5.js
+++ app/taskgroup3/task3_5.js
@@ -1,2 +1,8 @@
-let arr1 = [1, 2, [3, 4]];
-arr1.flat();
+let arr = [1, [2], [3, [4]]];
+function flatten(arr) {
+	return arr.join(',').split(',');
+}
+
+let res = flatten(arr);
+
+console.log(res);
